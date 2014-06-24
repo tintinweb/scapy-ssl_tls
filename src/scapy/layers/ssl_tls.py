7b5a13a9add9fa600cd9414ea51ad9379b024f7e
tintinweb <tintinweb@oststrom.com>
2014-06-24 20:48:48 +0200

    + added even more TLS protocol components and proper scapy layer linking

---
+++ src/scapy/layers/ssl_tls.py
@@ -0,0 +1,337 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Author : tintinweb@oststrom.com <github.com/tintinweb>
+# http://www.secdev.org/projects/scapy/doc/build_dissect.html
+from scapy.packet import Packet, bind_layers
+from scapy.fields import *
+from scapy.layers.inet import TCP
+import os, time
+
+class BLenField(LenField):
+    def __init__(self, name, default, fmt = "I", adjust=lambda pkt,x:x, numbytes=None, length_of=None, count_of=None):
+        self.name = name
+        self.adjust=adjust
+        self.numbytes=numbytes
+        self.length_of= length_of
+        self.count_of = count_of
+        LenField.__init__(self, name, default, fmt)
+
+        if fmt[0] in "@=<>!":
+            self.fmt = fmt
+        else:
+            self.fmt = "!"+fmt
+        self.default = self.any2i(None,default)
+        self.sz = struct.calcsize(self.fmt) if not numbytes else numbytes
+        self.owners = []
+        
+    def addfield(self, pkt, s, val):
+        """Add an internal value  to a string"""
+        pack = struct.pack(self.fmt, self.i2m(pkt,val))
+        if self.numbytes:
+            pack=pack[len(pack)-self.numbytes:]
+        return s+pack
+    def getfield(self, pkt, s):
+        """Extract an internal value from a string"""
+        upack_data = s[:self.sz]
+        # prepend struct.calcsize()-len(data) bytes to satisfy struct.unpack
+        upack_data = '\x00'*(struct.calcsize(self.fmt)-self.sz) + upack_data
+            
+        return  s[self.sz:], self.m2i(pkt, struct.unpack(self.fmt, upack_data)[0])
+    
+    def i2m(self, pkt, x):
+        if x is None:
+            if not (self.length_of or self.count_of):
+                 x = len(pkt.payload)
+                 return x
+             
+            if self.length_of is not None:
+                fld,fval = pkt.getfield_and_val(self.length_of)
+                f = fld.i2len(pkt, fval)
+            else:
+                fld,fval = pkt.getfield_and_val(self.count_of)
+                f = fld.i2count(pkt, fval)
+            x = self.adjust(pkt,f)
+        return x
+
+class XBLenField(BLenField):
+    def i2repr(self, pkt, x):
+        return lhex(self.i2h(pkt, x))
+    
+class XLenField(LenField):
+    def i2repr(self, pkt, x):
+        return lhex(self.i2h(pkt, x))
+    
+class XFieldLenField(FieldLenField):
+    def i2repr(self, pkt, x):
+        return lhex(self.i2h(pkt, x))    
+
+TLS_VERSIONS = {0x0300:"SSL_3_0",
+                  0x0301:"TLS_1_0",
+                  0x0302:"TLS_1_1",
+                  0x0303:"TLS_1_2"}
+
+TLS_CONTENT_TYPES = {0x14:"change_cipher_spec",
+                        0x15:"alert",
+                        0x16:"handshake",
+                        0x17:"application_data",
+                        0x18:"heartbeat",
+                        0xff:"unknown"}
+
+TLS_HANDSHAKE_TYPES = {0x00:"hello_request",
+                        0x01:"client_hello",
+                        0x02:"server_hello",
+                        0x0b:"certificate",
+                        0x0c:"server_key_exchange",
+                        0x0d:"certificate_request",
+                        0x0e:"server_hello_done",
+                        0x0f:"certificate_verify",
+                        0x10:"client_key_exchange",
+                        0x20:"finished",
+                        0x21:"certificate_url",
+                        0x22:"certificate_stats",
+                        0xff:"unknown"}
+
+TLS_EXTENSION_TYPES = {
+                       0x0000:"server_name",
+                       0x0023:"session_ticket_tls",
+                       0x000f:"heartbeat",
+                       0x000f:"status_request",
+                       0xff01:"renegotiationg_info",
+                       0x000d:"signature_algorithms",
+                       }
+
+TLS_ALERT_LEVELS = { 0x01: "warning",
+                     0x02: "fatal",
+                     0xff: "unknwon",}
+
+TLS_ALERT_DESCRIPTIONS = {    
+                    0:"CLOSE_NOTIFY",
+                    10:"UNEXPECTE_MESSAGE",
+                    20:"BAD_RECORD_MAC",
+                    21:"DESCRIPTION_FAILED_RESERVED",
+                    22:"RECORD_OVERFLOW",
+                    30:"DECOMPRESSION_FAILUR",
+                    40:"HANDSHAKE_FAILUR",
+                    41:"NO_CERTIFICATE_RESERVED",
+                    43:"BAD_CERTIFICATE",
+                    43:"UNSUPPORTED_CERTIFICATE",
+                    44:"CERTIFICATE_REVOKED",
+                    45:"CERTIFICATE_EXPIRED",
+                    46:"CERTIFICATE_UNKNOWN",
+                    47:"ILLEGAL_PARAMETER",
+                    48:"UNKNOWN_CA",
+                    49:"ACCESS_DENIED",
+                    50:"DECODE_ERROR",
+                    51:"DECRYPT_ERROR",
+                    60:"EXPORT_RESTRICTION_RESERVED",
+                    70:"PROTOCOL_VERSION",
+                    71:"INSUFFICIENT_SECURITY",
+                    80:"INTERNAL_ERROR",
+                    90:"USER_CANCELED",
+                    100:"NO_RENEGOTIATION",
+                    110:"UNSUPPORTED_EXTENSION",
+                    255:"UNKNOWN_255",}
+
+
+
+class TLSCipherSuite:
+    '''
+    make ciphersuites available as class props (autocompletion)
+    '''
+    NULL_WITH_NULL_NULL = 0x0000
+    RSA_WITH_NULL_MD5 = 0x0001
+    RSA_WITH_NULL_SHA1 = 0x0002
+    RSA_WITH_NULL_SHA256 = 0x003b
+    RSA_WITH_3DES_EDE_CBC_SHA =  0x000a
+    DHE_RSA_WITH_3DES_EDE_CBC_SHA  = 0x0016    
+    DHE_DSS_WITH_3DES_EDE_CBC_SHA  = 0x0013
+    RSA_WITH_3DES_EDE_CBC_SHA =  0x000a
+    DHE_RSA_WITH_AES_128_CBC_SHA  =  0x0033
+    DHE_DSS_WITH_AES_128_CBC_SHA  = 0x0032
+    RSA_WITH_AES_128_CBC_SHA = 0x002f
+    RSA_WITH_IDEA_CBC_SHA  = 0x0007
+    DHE_DSS_WITH_RC4_128_SHA  = 0x0066
+    RSA_WITH_RC4_128_SHA  = 0x0005
+    RSA_WITH_RC4_128_MD5  = 0x0004
+    DHE_DSS_EXPORT1024_WITH_DES_CBC_SHA  = 0x0063
+    RSA_EXPORT1024_WITH_DES_CBC_SHA  = 0x0062
+    RSA_EXPORT1024_WITH_RC2_CBC_56_MD5  = 0x0061
+    DHE_RSA_WITH_DES_CBC_SHA  = 0x0015
+    DHE_DSS_WITH_DES_CBC_SHA  = 0x0012
+    RSA_WITH_DES_CBC_SHA  = 0x0009
+    DHE_DSS_EXPORT1024_WITH_RC4_56_SHA  = 0x0065
+    RSA_EXPORT1024_WITH_RC4_56_SHA  = 0x0064
+    RSA_EXPORT1024_WITH_RC4_56_MD5  = 0x0060
+    DHE_RSA_EXPORT_WITH_DES40_CBC_SHA  = 0x0014
+    DHE_DSS_EXPORT_WITH_DES40_CBC_SHA  = 0x0011
+    RSA_EXPORT_WITH_DES40_CBC_SHA  = 0x0008
+    RSA_EXPORT_WITH_RC2_CBC_40_MD5  = 0x0006
+    RSA_EXPORT_WITH_RC4_40_MD5  = 0x0003
+    RSA_WITH_AES_256_CBC_SHA = 0x0035
+    DHE_DSS_WITH_AES_256_CBC_SHA = 0x0038    
+    DHE_RSA_WITH_AES_256_CBC_SHA = 0x0039
+    ECDHE_ECDSA_WITH_AES_256_CBC_SHA = 0xc00a
+    ECDH_RSA_WITH_AES_256_CBC_SHA = 0xc00f    
+    ECDHE_RSA_WITH_AES_256_CBC_SHA  = 0xc014
+    SRP_SHA_RSA_WITH_AES_256_CBC_SHA = 0xc021
+    SRP_SHA_DSS_WITH_AES_256_CBC_SHA = 0xc022
+    DHE_DSS_WITH_CAMELLIA_256_CBC_SHA = 0x0087
+    DHE_RSA_WITH_CAMELLIA_256_CBC_SHA = 0x0088
+    ECDH_ECDSA_WITH_AES_256_CBC_SHA =0xc005
+    RSA_WITH_CAMELLIA_256_CBC_SHA = 0x0084
+    
+TLS_CIPHER_SUITES = dict((v,k) for k,v in TLSCipherSuite.__dict__.items() if not k.startswith("__"))
+
+class TLSCompressionMethod:
+    '''
+    make compression methods available as class props (autocompletion)
+    '''
+    NULL = 0x00
+    DEFLATE = 0x01
+    
+TLS_COMPRESSION_METHODS= dict((v,k) for k,v in TLSCompressionMethod.__dict__.items() if not k.startswith("__"))
+
+class TLSRecord(Packet):
+    name = "TLS Record"
+    fields_desc = [ByteEnumField("content_type", 0xff, TLS_CONTENT_TYPES),
+                   XShortEnumField("version", 0x0301, TLS_VERSIONS),
+                   XLenField("length",None, fmt="!H"),]
+    
+class TLSHandshake(Packet):
+    name = "TLS Handshake"
+    fields_desc = [ByteEnumField("type", 0xff, TLS_HANDSHAKE_TYPES),
+                   XBLenField("length",None, fmt="!I", numbytes=3),]
+
+
+class TLSServerName(Packet):
+    name = "TLS Servername"
+    fields_desc = [ByteEnumField("type", 0x00, {0x00:"host"}),
+                  XFieldLenField("length",None,length_of="data",fmt="H"),
+                  StrLenField("data","",length_from=lambda x:x.length),
+                  ]
+    
+class TLSServerNameIndication(Packet):
+    name = "TLS Extension Servername Indication"
+    fields_desc = [XFieldLenField("length",None,length_of="server_names",fmt="H"),
+                   PacketListField("server_names",None,TLSServerName(),length_from=lambda x:x.length),
+                  ]
+
+class TLSExtension(Packet):
+    name = "TLS Extension"
+    fields_desc = [XShortEnumField("type", 0x0000, TLS_EXTENSION_TYPES),
+                   XLenField("length",None, fmt="!H"),
+                  ]
+
+class TLSClientHello(Packet):
+    name = "TLS Client Hello"
+    fields_desc = [XShortEnumField("version", 0x0301, TLS_VERSIONS),
+                   IntField("gmt_unix_time",int(time.time())),
+                   StrFixedLenField("random_bytes",os.urandom(28),28),
+                   XFieldLenField("session_id_length",None,length_of="session_id",fmt="B"),
+                   StrLenField("session_id",'',length_from=lambda x:x.session_id_length),
+                   
+                   XFieldLenField("cipher_suites_length",None,length_of="cipher_suites",fmt="H"),
+                   FieldListField("cipher_suites",None,XShortEnumField("cipher",None,TLS_CIPHER_SUITES),length_from=lambda x:x.cipher_suites_length),
+                   
+                   XFieldLenField("compression_methods_length",None,length_of="compression_methods",fmt="B"),
+                   FieldListField("compression_methods",None,ByteEnumField("compression",None,TLS_COMPRESSION_METHODS), length_from=lambda x:x.compression_methods_length),
+                   
+                   XFieldLenField("extensions_length",None,length_of="extensions",fmt="H"),
+                   PacketListField("extensions",None,TLSExtension(), length_from=lambda x:x.extension_length),
+                   ]
+    
+class TLSServerHello(Packet):
+    name = "TLS Server Hello"
+    fields_desc = [XShortEnumField("version", 0x0301, TLS_VERSIONS),
+                   IntField("gmt_unix_time",int(time.time())),
+                   StrFixedLenField("random_bytes",os.urandom(28),28),
+                   XFieldLenField("session_id_length",None,length_of="session_id",fmt="B"),
+                   StrLenField("session_id",'',length_from=lambda x:x.session_id_length),
+
+                   XShortEnumField("cipher_suite", 0x0000, TLS_CIPHER_SUITES),
+                   ByteEnumField("compression_method", 0x00, TLS_COMPRESSION_METHODS),
+
+                   XFieldLenField("extensions_length",None,length_of="extensions",fmt="H"),
+                   PacketListField("extensions",None,TLSExtension(), length_from=lambda x:x.extension_length),
+                   ]
+
+class TLSAlert(Packet):
+    name = "TLS Alert"
+    fields_desc = [ByteEnumField("level", 0xff, TLS_ALERT_LEVELS),
+                  ByteEnumField("description", 0xff, TLS_ALERT_DESCRIPTIONS),
+                  ]
+
+
+class TLSHeartBeat(Packet):
+    name = "TLS Extension HeartBeat"
+    fields_desc = [ByteEnumField("type", 0x01, {0x01:"request"}),
+                  FieldLenField("length",None,length_of="data",fmt="H"),
+                  StrLenField("data","",length_from=lambda x:x.length),
+                  StrLenField("padding","", length_from=lambda x: 'P'*(16-x.length)),
+                  ]
+
+class TLSServerKeyExchange(Packet):
+    name = "TLS Server Key Exchange"
+    fields_desc = [ XBLenField("length",None, fmt="!I", numbytes=3),
+                    StrLenField("data",os.urandom(329),length_from=lambda x:x.length),]
+
+class TLSServerHelloDone(Packet):
+    name = "TLS Server Hello Done"
+    fields_desc = [ XBLenField("length",None, fmt="!I", numbytes=3),
+                    StrLenField("data","",length_from=lambda x:x.length),]
+class TLSCertificate(Packet):
+    name = "TLS Certificate"
+    fields_desc = [ XBLenField("length",None, length_of="data", fmt="!I", numbytes=3),
+                    StrLenField("data","",length_from=lambda x:x.length),]
+    
+class TLSCertificateList(Packet):
+    name = "TLS Certificate List"
+    fields_desc = [
+                   XBLenField("length",None,length_of="certificates",fmt="!I", numbytes=3),
+                   PacketListField("certificates",None,TLSCertificate(), length_from=lambda x:x.length),
+                  ]   
+
+class TLSChangeCipherSpec(Packet):
+    name = "TLS ChangeCipherSpec"
+    fields_desc = [ StrField("message",None, fmt="H")]
+
+
+
+# entry class
+class SSL(Packet):
+    name = "SSL"
+    def do_dissect(self, s):
+        return s
+
+    def guess_payload_class(self, payload):
+        try:
+            raise
+        except:
+            pass
+        return Packet.guess_payload_class(self, payload)
+
+
+
+# bind magic
+bind_layers(TCP, SSL, dport=443)
+bind_layers(TLSRecord, TLSAlert, {'content_type':0x15})
+
+
+bind_layers(TLSRecord, TLSHandshake, {'content_type':0x16})
+# --> handshake proto
+bind_layers(TLSHandshake,TLSClientHello, {'type':0x01})
+bind_layers(TLSHandshake,TLSServerHello, {'type':0x02})
+bind_layers(TLSHandshake,TLSCertificate, {'type':0x0b})
+# <---
+
+# --> extensions
+bind_layers(TLSExtension,TLSServerNameIndication, {'type': 0x0000})
+
+# <--
+
+bind_layers(TLSRecord, TLSChangeCipherSpec, {'content_type':0x14})
+bind_layers(TLSRecord, TLSHeartBeat, {'content_type':0x18})
+
+
+
+
