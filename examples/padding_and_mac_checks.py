98580dcb529bc9f0c65227800f6b57ed7aeb3e83
Alex Moneger <alex.moneger@citrix.com>
2015-08-24 11:05:57 -0700

    Modified the POODLE2 test case to test all bytes
    - All bytes of padding are now checked
    - All bytes of the mac are also checked

---
+++ examples/padding_and_mac_checks.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+from __future__ import print_function
+import os
+import sys
+
+try:
+    # This import works from the project directory
+    basedir = os.path.abspath(os.path.join(os.path.dirname(__file__),"../"))
+    sys.path.append(basedir)
+    from scapy_ssl_tls.ssl_tls import *
+    from scapy_ssl_tls.ssl_tls_crypto import *
+except ImportError:
+    # If you installed this package via pip, you just need to execute this
+    from scapy.layers.ssl_tls import *
+    from scapy.layers.ssl_tls_crypto import *
+
+
+index = 0
+
+
+def modify_padding(crypto_container):
+    padding = crypto_container.padding
+    crypto_container.padding = "%s\xff%s" % (padding[:index], padding[index + 1:])
+    return crypto_container
+
+
+def modify_mac(crypto_container):
+    mac = crypto_container.mac
+    crypto_container.mac = "%s\xff%s" % (mac[:index], mac[index + 1:])
+    return crypto_container
+
+
+def send_application_data(server, cipher_suite, data, hook):
+    s = socket.socket()
+    s.connect(server)
+    tls_socket = TLSSocket(s, client=True)
+    version = TLSVersion.TLS_1_0
+    tls_do_handshake(tls_socket, version, cipher_suite)
+    tls_socket.sendall(to_raw(TLSPlaintext(data=data), tls_socket.tls_ctx, pre_encrypt_hook=hook))
+    r = tls_socket.recvall()
+    return r
+
+
+def align_data_on_block_bounday(data, cipher_suite, pad_char="a"):
+    data_len = len(data)
+    block_len = TLSSecurityParameters.crypto_params[cipher_suite]["cipher"]["type"].block_size
+    mac_len = TLSSecurityParameters.crypto_params[cipher_suite]["hash"]["type"].digest_size
+    junk_len = block_len - ((data_len + mac_len) % block_len)
+    return "%s%s" % (data, pad_char * junk_len)
+
+
+def test_all_field_bytes(server, cipher_suite, block_aligned_request, modify_padding):
+    error_msg = ""
+    try:
+        resp = send_application_data(server, cipher_suite, block_aligned_request, modify_padding)
+        if len(resp.records) == 0:
+            error_msg = "Server is not vulnerable, but implementation does not send a BAD_RECORD_MAC alert"
+        elif resp.haslayer(TLSAlert) and resp[TLSAlert].description == TLSAlertDescription.BAD_RECORD_MAC:
+            error_msg = "Server is not vulnerable"
+        else:
+            error_msg = "Server is probably vulnerable\n"
+            error_msg += "If application data was displayed above, server is definitely vulnerable"
+            resp.show()
+    except socket.error:
+        error_msg += "Connection reset by peer"
+    return error_msg
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        server = (sys.argv[1], int(sys.argv[2]))
+    else:
+        server = ("127.0.0.1", 8443)
+    cipher_suite = TLSCipherSuite.RSA_WITH_AES_128_CBC_SHA
+
+    request = "GET / HTTP/1.1\r\nHOST: %s\r\n\r\n" % server[0]
+    block_aligned_request = align_data_on_block_bounday(request, cipher_suite)
+
+    print("Testing all padding bytes")
+    # Perform poodle 2 check
+    for _ in range(0, TLSSecurityParameters.crypto_params[cipher_suite]["cipher"]["type"].block_size - 1):
+        print("Modifying padding byte %d" % index)
+        print(test_all_field_bytes(server, cipher_suite, block_aligned_request, modify_padding))
+        index += 1
+
+    print("Testing all mac bytes")
+    index = 0
+    # Perform mac check
+    for _ in range(0, TLSSecurityParameters.crypto_params[cipher_suite]["hash"]["type"].digest_size - 1):
+        print("Modifying mac byte %d" % index)
+        print(test_all_field_bytes(server, cipher_suite, block_aligned_request, modify_mac))
+        index += 1
+
+    print("Test complete")
