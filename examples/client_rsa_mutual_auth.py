9760056af06d45150f333b14e9828c999347dbfa
Alex Moneger <alex.moneger@citrix.com>
2017-12-19 13:37:35 -0800

    Update mutual auth example to new interface
    - Renew expired client side certs

--- examples/client_rsa_mutual_auth.py
+++ examples/client_rsa_mutual_auth.py
@@ -4,10 +4,10 @@
 from __future__ import with_statement
 from __future__ import print_function
 import os
-import socket
-import sys
 
-from Cryptodome.Hash import SHA256
+
+import Cryptodome
+
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 try:
@@ -27,49 +27,44 @@ def do_tls_mutual_auth(host):
 
     tls_version = TLSVersion.TLS_1_2
 
-    socket_ = socket.socket()
-    tls_socket = TLSSocket(socket_, client=True)
-    tls_socket.connect(host)
-    tls_socket.tls_ctx.client_ctx.load_rsa_keys_from_file(os.path.join(
-        basedir, "tests/integration/keys/scapy-tls-client.key.pem"))
+    with TLSSocket(socket.socket(), client=True) as tls_socket:
+        tls_socket.connect(host)
+        tls_socket.tls_ctx.client_ctx.load_rsa_keys_from_file(os.path.join(
+            basedir, "tests/integration/keys/scapy-tls-client.key.pem"))
 
-    client_hello = TLSRecord(version=tls_version) / \
-                   TLSHandshakes(handshakes=[TLSHandshake() /
-                                             TLSClientHello(version=tls_version,
-                                                            compression_methods=[TLSCompressionMethod.NULL, ],
-                                                            cipher_suites=[TLSCipherSuite.ECDHE_RSA_WITH_AES_128_CBC_SHA256, ])])
-    tls_socket.sendall(client_hello)
-    server_hello = tls_socket.recvall()
-    server_hello.show()
+        client_hello = TLSRecord(version=tls_version) / \
+                       TLSHandshakes(handshakes=[TLSHandshake() /
+                                                 TLSClientHello(version=tls_version,
+                                                                cipher_suites=[TLSCipherSuite.ECDHE_RSA_WITH_AES_128_CBC_SHA256])])
+        server_hello = tls_socket.do_round_trip(client_hello)
+        # server_hello.show()
 
-    client_cert = TLSRecord(version=tls_version) / \
-                  TLSHandshakes(handshakes=[TLSHandshake() /
-                                            TLSCertificateList(certificates=certificate)])
-    client_key_exchange = TLSRecord(version=tls_version) / \
-                          TLSHandshakes(handshakes=[TLSHandshake() /
-                                                    tls_socket.tls_ctx.get_client_kex_data()])
-    p = TLS.from_records([client_cert, client_key_exchange])
-    tls_socket.sendall(p)
+        client_cert = TLSRecord(version=tls_version) / \
+                      TLSHandshakes(handshakes=[TLSHandshake() / TLSCertificateList() /
+                                                TLS10Certificate(certificates=certificate)])
+        client_key_exchange = TLSRecord(version=tls_version) / \
+                              TLSHandshakes(handshakes=[TLSHandshake() /
+                                                        tls_socket.tls_ctx.get_client_kex_data()])
+        p = TLS.from_records([client_cert, client_key_exchange])
+        tls_socket.do_round_trip(p, recv=False)
 
-    sig = tls_socket.tls_ctx.get_client_signed_handshake_hash(SHA256.new())
-    # sig = sig[:128] + chr(ord(sig[128]) ^ 0xff) + sig[129:]
-    client_cert_verify = TLSRecord(version=tls_version) / \
-                         TLSHandshakes(handshakes=[TLSHandshake() /
-                                                   TLSCertificateVerify(alg=TLSSignatureScheme.RSA_PKCS1_SHA256,
-                                                                        sig=sig)])
-    tls_socket.sendall(client_cert_verify)
+        sig = tls_socket.tls_ctx.compute_client_cert_verify(digest=Cryptodome.Hash.SHA256)
 
-    client_ccs = TLSRecord(version=tls_version) / TLSChangeCipherSpec()
-    tls_socket.sendall(client_ccs)
-    tls_socket.sendall(to_raw(TLSFinished(), tls_socket.tls_ctx))
-    server_finished = tls_socket.recvall()
-    server_finished.show()
+        client_cert_verify = TLSRecord(version=tls_version) / \
+                             TLSHandshakes(handshakes=[TLSHandshake() /
+                                                       TLSCertificateVerify(alg=TLSSignatureScheme.RSA_PKCS1_SHA256,
+                                                                            sig=sig)])
+        tls_socket.do_round_trip(client_cert_verify, recv=False)
 
-    tls_socket.sendall(to_raw(TLSPlaintext(data="GET / HTTP/1.1\r\nHOST: localhost\r\n\r\n"), tls_socket.tls_ctx))
-    resp = tls_socket.recvall()
-    print("Got response from server")
-    resp.show()
-    print(tls_socket.tls_ctx)
+        client_ccs = TLSRecord(version=tls_version) / TLSChangeCipherSpec()
+        tls_socket.do_round_trip(client_ccs, recv=False)
+        server_finished = tls_socket.do_round_trip(TLSHandshakes(handshakes=[TLSHandshake() / TLSFinished(data=tls_socket.tls_ctx.get_verify_data())]))
+        server_finished.show()
+
+        resp = tls_socket.do_round_trip(TLSPlaintext(data="GET / HTTP/1.1\r\nHOST: localhost\r\n\r\n"))
+        print("Got response from server")
+        resp.show()
+        print(tls_socket.tls_ctx)
 
 
 if __name__ == "__main__":
